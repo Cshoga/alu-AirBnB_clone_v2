cc1dc01537173d4bec77879f665807b8683b6066
tuyishime johnson <j.tuyishime4@alustudent.com>
2023-10-19 18:44:31 +0200

    Python is cool

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -20,8 +20,6 @@ def hello_c(text):
     return 'C ' + text.replace('_', ' ')
 
 @app.route('/python/', defaults={'text': "is cool"}, strict_slashes=False)
-
-
 @app.route('/python/<text>', strict_slashes=False)
 def changing_python(text):
     return "Python " + text.replace('_', ' ')
