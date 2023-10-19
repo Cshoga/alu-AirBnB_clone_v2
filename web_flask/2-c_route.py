2843921720fc9f1b701400d2332dacec8ba48884
tuyishime johnson <j.tuyishime4@alustudent.com>
2023-10-19 17:32:05 +0200

    C is fun

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -1,19 +1,25 @@
 #!/usr/bin/python3
 """ Script that displays C followed by the value of the text variable """
 from flask import Flask
+
+
 app = Flask(__main__)
 
-@app.route('/', strict_slashes=False)
+
+@app.route('/')
 def index():
     return "Hello HBNB"
 
-@app.route('/hbnb', strict_slashes=False)
+
+@app.route('/hbnb')
 def hbnb():
     return "HBNB"
+
 
 @app.route('/c/<text>')
 def change_name(text):
     changed_text = text.replace('_', ' ')
+    return changed_text
     
 
 if __name__=="__main__":
